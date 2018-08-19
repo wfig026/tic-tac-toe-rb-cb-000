06b97dd6da43732c59a244224ef85f59cf01865e
Will Figueiredo <will@wfigueiredo.com>
2018-08-19 20:35:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ def turn(board)
   index = input_to_index(input)
 
   if valid_move?(board,index)
-    move(board,index)
+    move(board,index, character)
     display_board(board)
   else
     turn(board)
