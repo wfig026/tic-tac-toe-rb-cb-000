e9c6867ba1d420baf06f2aad668d0f6983cfb25b
Will Figueiredo <will@wfigueiredo.com>
2018-08-19 20:19:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,3 +20,13 @@ def display_board(board)
   puts "-" * 11
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+# Is position taken?
+def position_taken?(board, index)
+  position = board[index]
+  if position == " " || position == "" || position == nil
+    return false
+  else
+    return true
+  end
+end
